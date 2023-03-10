b7640754164e1a488c18d19257b9dafcaccc54fa
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-03-10 18:03:16 +0700

    get data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:project_1/page.dart';
+// import 'package:project_1/page.dart';
+import 'package:project_1/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,50 +12,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: AppBarExample(),
+      home: Home(),
     );
   }
 }
 
-class AppBarExample extends StatelessWidget {
-  const AppBarExample({super.key});
+// class AppBarExample extends StatelessWidget {
+//   const AppBarExample({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('AppBar Demo'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'Show Snackbar',
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('This is a snackbar')));
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.navigate_next),
-            tooltip: 'Go to the next page',
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute<void>(
-                  builder: (BuildContext context) {
-                    return const PageNext();
-                  },
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text(
-          'This is the home page',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('AppBar Demo'),
+//         actions: <Widget>[
+//           IconButton(
+//             icon: const Icon(Icons.add_alert),
+//             tooltip: 'Show Snackbar',
+//             onPressed: () {
+//               ScaffoldMessenger.of(context).showSnackBar(
+//                   const SnackBar(content: Text('This is a snackbar')));
+//             },
+//           ),
+//           IconButton(
+//             icon: const Icon(Icons.navigate_next),
+//             tooltip: 'Go to the next page',
+//             onPressed: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute<void>(
+//                   builder: (BuildContext context) {
+//                     return const PageNext();
+//                   },
+//                 ),
+//               );
+//             },
+//           ),
+//         ],
+//       ),
+//       body: const Center(
+//         child: Text(
+//           'This is the home page',
+//           style: TextStyle(fontSize: 24),
+//         ),
+//       ),
+//     );
+//   }
+// }
